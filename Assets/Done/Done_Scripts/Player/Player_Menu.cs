60b769e72d69ccdb4daf6ce3a59a33e2d1c95433
LipingSun <liping.sun@sjsu.edu>
2015-05-07 14:53:02 -0700

    Dev 2

--- Assets/Done/Done_Scripts/Player/Player_Menu.cs
+++ Assets/Done/Done_Scripts/Player/Player_Menu.cs
@@ -6,13 +6,13 @@ public class Player_Menu : MonoBehaviour {
 	void OnGUI()
 	{
 		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 16, Screen.width / 5, Screen.height / 15), "Level 1")) {
-			Application.LoadLevel("Game_Level_1");
+			Application.LoadLevel("Game");
 		}
 		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 8, Screen.width / 5, Screen.height / 15), "Level 2")) {
-			Application.LoadLevel("Game_Level_2");
+			Application.LoadLevel("Game");
 		}
 		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 4, Screen.width / 5, Screen.height / 15), "Level 3")) {
-			Application.LoadLevel("Game_Level_3");
+			Application.LoadLevel("Game");
 		}
 		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 2, Screen.width / 5, Screen.height / 10), "Return")) {
 			Application.LoadLevel("StartMenu");
